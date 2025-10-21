082b660c90dfc31e5e46ec3575a67504e8428429
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-10-21 11:55:47 +0530

    Db file

---
+++ backend/src/db/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/food-view') 
+        .then(()=>{
+            console.log("Connected to MongoDB successfully");
+        })
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+module.exports = connectDB;
