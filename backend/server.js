a25d056c7b25bb194e8f74d61bf7da7662af96db
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-10-21 11:55:17 +0530

    server file setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const app = require("./src/app");
-
+const connectDB = require("./src/db/db");
 //start server
+connectDB();
 app.listen(3000, ()=> {
     console.log("server is running mon on port 3000");
 })
