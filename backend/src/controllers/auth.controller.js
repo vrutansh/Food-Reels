a5ab370b08cf8998e454e89ef11fdd575a5d1c94
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-10-23 21:38:02 +0530

    logout added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -69,7 +69,18 @@ const loginUser = async (req, res) => {
         }
     });
 }
+
+// User Logout Controller
+const logoutUser = async (req, res) => {
+    res.clearCookie("token");
+    res.status(200).json({ message: "Logout successful" }); 
+    
+}
+
 module.exports = {
     registerUser,
     loginUser,
+    logoutUser,
 };
+
+
