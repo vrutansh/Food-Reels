a867066ac02f39d429b911fca58b9a489d6b23fb
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-10-25 21:19:22 +0530

    routes update

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,10 +7,12 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 const authRoutes = require('./routes/auth.routes')
-
+const foodRoutes = require('./routes/food.routes')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(cookieParser());
 
 app.use('/api/auth',authRoutes);
+app.use('/api/food', foodRoutes );
+
 module.exports = app;
