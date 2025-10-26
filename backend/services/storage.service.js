f81231ec251ece8d15fca80d7284e103598278c3
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-10-26 21:31:21 +0530

    multer storage added

---
+++ backend/services/storage.service.js
@@ -0,0 +1,7 @@
+const Imagekit = require("imagekit");
+
+const  imagekit = new Imagekit({
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+});
