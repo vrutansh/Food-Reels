575af1b019b11b4bb88816d40e5bd9b8a90d6dc3
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-11-05 21:39:05 +0530

    updt

--- backend/src/controllers/foodpartner.controller.js
+++ backend/src/controllers/foodpartner.controller.js
@@ -1,25 +1,29 @@
 const foodPartnerModel = require('../models/foodpartner.model');
 const foodModel = require('../models/food.model');
 async function getFoodPartnerById(req, res) {
-    try {
-        const foodPartnerId = req.params.id;
-        const foodItemsByFoodpartner = await foodModel.find({ foodPartner: foodPartnerId });
-        const foodPartner = await foodPartnerModel.findById(foodPartnerId);
+ try {
+    const foodPartnerId = req.params.id;
 
-        if (!foodPartner) {
-            return res.status(404).json({ message: 'Food partner not found' });
-        }           
-        res.status(200).json({
-            message: 'Food partner fetched successfully',
-            foodPartner:{
-                ...foodPartner.toObject(),
-                foodItems: foodItemsByFoodpartner
-            }
-             });
-    } catch (error) {
-        console.error('Error fetching food partner by ID:', error);
-        res.status(500).json({ message: 'Internal server error' });
-    }  
+    const foodPartner = await foodPartnerModel.findById(foodPartnerId)
+    const foodItemsByFoodPartner = await foodModel.find({ foodPartner: foodPartnerId })
+
+    if (!foodPartner) {
+        return res.status(404).json({ message: "Food partner not found" });
+    }
+
+    res.status(200).json({
+        message: "Food partner retrieved successfully",
+        foodPartner: {
+            ...foodPartner.toObject(),
+            foodItems: foodItemsByFoodPartner
+        }
+
+    });
+} 
+catch (error) {
+    console.error("Error fetching food partner by ID:", error);
+    res.status(500).json({ message: "Internal server error" });
+}
 }
 
 module.exports = {
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -8,17 +8,20 @@ import FoodPartnerLogin from '../pages/auth/FoodPartnerLogin.jsx';
 import Home from '../pages/general/Home.jsx';
 import CreateFood from '../pages/food-partner/CreateFood.jsx';
 import Profile from '../pages/food-partner/Profile.jsx';
+import ChooseRegister from '../pages/auth/ChooseRegister.jsx';
+import BottomNav from '../components/BottomNav.jsx';
 const AppRoutes = () => {
   return (
     <div>
       
       <Router>
         <Routes >
+                <Route path="/register" element={<ChooseRegister />} />
                 <Route path="/user/register" element={<UserRegister />} />
                 <Route path="/user/login" element={<UserLogin />} />
                 <Route path="/food-partner/register" element={<FoodPartnerRegister />} />
                 <Route path="/food-partner/login" element={<FoodPartnerLogin />} />
-                <Route path="/" element={<Home/>} />
+                <Route path="/" element={<><Home /><BottomNav /></>} />
                 <Route path="create-food" element={<CreateFood/>} />
                 <Route path="/food-partner/:id" element={<Profile/>} />
         </Routes>
