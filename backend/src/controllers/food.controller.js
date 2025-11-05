0b75366b6f453603db56ac0763a80e8afa3eebef
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-11-05 22:47:08 +0530

    added like ft

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -1,4 +1,7 @@
 const foodModel = require('../models/food.model');
+const like = require('../models/likes.model');
+const likeModel = require('../models/likes.model');
+const saveModel = require('../models/save.model');
 const  storageService  = require('../services/storage.service');
 
 const { v4: uuid } = require('uuid');
@@ -31,7 +34,81 @@ async function getAllFoodItems(req, res) {
     });
 }
 
+async function likeFoodItem(req, res) {
+    const {foodId }= req.body;
+    const user = req.user;
+
+    const isAlreadyLiked = await likeModel.findOne({
+        user: user._id,
+        food: foodId,
+    })
+
+    if (isAlreadyLiked) {
+        await likeModel.deleteOne({
+            user: user._id,
+            food: foodId,
+        });
+        
+        await foodModel.findByIdAndUpdate(foodId, {
+            $inc: { likeCount: -1 },
+        });
+
+        res.status(201).json({
+        message: "Food unliked successfully",
+        like: like,
+    }); 
+    }
+
+    const like = await likeModel.create({
+        user: user._id,
+        food: foodId,
+    });
+
+     await foodModel.findByIdAndUpdate(foodId, {
+            $inc: { likeCount: 1 },
+        });
+
+    res.status(201).json({
+        message: "Food liked successfully",
+        like: like,
+    });
+
+}
+
+async function saveFood(req, res) {
+    // Implementation for saving a food item goes here
+     const {foodId }= req.body;
+    const user = req.user;
+
+    const isAlreadysaved = await saveModel.findOne({
+        user: user._id,
+        food: foodId,
+    })
+
+    if (isAlreadysaved) {
+        await saveModel.deleteOne({
+            user: user._id,
+            food: foodId,
+        }); 
+
+        res.status(201).json({
+        message: "Food unsaved successfully",
+        })
+
+        const save = await saveModel.create({
+        user: user._id,
+        food: foodId,
+      });
+
+    res.status(201).json({
+        message: "Food saved successfully",
+        save: save,
+    });
+    }
+}
 module.exports = {
     createFood,
     getAllFoodItems,
-}
+    likeFoodItem,
+    saveFood,
+};
--- backend/src/models/food.model.js
+++ backend/src/models/food.model.js
@@ -16,7 +16,13 @@ const foodSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'foodpartner',
         required: true,
-    }
+    },
+    likeCount: {
+        type: Number,
+        default: 0,
+    },
+}, {timestamps: true,
+
 });
 
 const foodModel = mongoose.model('food', foodSchema);
