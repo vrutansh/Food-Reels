507432d5207174e9f957046eebc7574429a02454
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-12-05 12:32:35 +0530

    vercel json added

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-
 import UserRegister from '../pages/auth/UserRegister.jsx';
 import UserLogin from '../pages/auth/UserLogin.jsx';
 import FoodPartnerRegister from '../pages/auth/FoodPartnerRegister.jsx';
